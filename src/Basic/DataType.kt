37bf2f764d5e72871b1faaaaaa20c9860503b0b1
Prashant1503 <Prashant1998vani@gmail.com>
2019-06-15 15:16:04 +0530

    Initial commit

---
+++ src/Basic/DataType.kt
@@ -0,0 +1,22 @@
+package Basic
+
+fun main() {
+
+    val name : String = "Prashant"
+    val age  :Int = 21;
+    val semester : Int = 6;
+    val PassedORNot :Boolean = true
+    val Marks = 7.21
+    val CollegeName :String = "Parul university"
+
+    println("My name is ${name} and my age is ${age}, I am currently studing in ${CollegeName} in ${semester} " +
+            "and I have scored $Marks in 5th sem,I have {$PassedORNot}")
+
+
+
+
+
+
+
+
+}
---
+++ src/Basic/Funclass.kt
@@ -0,0 +1,21 @@
+package Basic
+
+fun main() {
+
+
+
+
+    var personObj = Person()          //Object ...
+    personObj.name = "Prahsnat"
+
+    print("My name is ${personObj.name}")
+
+
+
+}
+
+class Person {
+
+    var name = ""
+
+    }
